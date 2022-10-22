5ae547c5c10ab9f578a7cb8ff9b1c38d67b21dfc
Patel, Prit V <ppatel493@gatech.edu>
2022-10-22 14:05:17 -0400

    UserProfile Table Creation WIP

---
+++ Backend--JIA-2113---Language-Exchange-Matchmaker/src/migrations/create-UserProfile.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UserProfile', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'UserAccount',
+            key: 'id'
+        }
+      },
+        native_language: {
+            type: Sequelize.STRING
+        },
+        target_language: {
+            type: Sequelize.STRING
+        },
+        target_language_proficiency: {
+            type: Sequelize.STRING
+        },
+        age: {
+            type: Sequelize.INTEGER
+        },
+        gender: {
+            allowNull: true,
+            type: Sequelize.STRING
+        },
+        profession: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        hobby: {
+            allowNull: true,
+            type: Sequelize.STRING
+        }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UserProfile');
+  }
+};
