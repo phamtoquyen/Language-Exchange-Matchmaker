4330c45a055415b692021a08053dec7aa4442ca7
Christian Lee <christianlee@Christians-MacBook-Air-3.local>
2023-10-13 17:40:19 -0400

    Preliminary video functionality

---
+++ Frontend/src/Components/VideoRoom.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import { VideoPlayer } from './VideoPlayer';
+
+const APP_ID = 'e7152fbb977b40a08d30ad8ae9a436c5';
+const TOKEN =
+  '007eJxTYHDlZTZ69/9LisjbydGr+w1exnv/dL5ct6ruC4NeVt66/DUKDKnmhqZGaUlJlubmSSYGiQYWKcYGiSkWiamWiSbGZsmmlds0UxsCGRl4lzYxMzJAIIjPwpCTmZfNwAAAbwIfrA==';
+const CHANNEL = 'link';
+
+const client = AgoraRTC.createClient({
+  mode: 'rtc',
+  codec: 'vp8',
+});
+
+export const VideoRoom = () => {
+  const [users, setUsers] = useState([]);
+  const [localTracks, setLocalTracks] = useState([]);
+
+  const handleUserJoined = async (user, mediaType) => {
+    await client.subscribe(user, mediaType);
+
+    if (mediaType === 'video') {
+      setUsers((previousUsers) => [...previousUsers, user]);
+    }
+
+    if (mediaType === 'audio') {
+      // user.audioTrack.play()
+    }
+  };
+
+  const handleUserLeft = (user) => {
+    setUsers((previousUsers) =>
+      previousUsers.filter((u) => u.uid !== user.uid)
+    );
+  };
+
+  useEffect(() => {
+    client.on('user-published', handleUserJoined);
+    client.on('user-left', handleUserLeft);
+
+    client
+      .join(APP_ID, CHANNEL, TOKEN, null)
+      .then((uid) =>
+        Promise.all([
+          AgoraRTC.createMicrophoneAndCameraTracks(),
+          uid,
+        ])
+      )
+      .then(([tracks, uid]) => {
+        const [audioTrack, videoTrack] = tracks;
+        setLocalTracks(tracks);
+        setUsers((previousUsers) => [
+          ...previousUsers,
+          {
+            uid,
+            videoTrack,
+            audioTrack,
+          },
+        ]);
+        client.publish(tracks);
+      });
+
+    return () => {
+      for (let localTrack of localTracks) {
+        localTrack.stop();
+        localTrack.close();
+      }
+      client.off('user-published', handleUserJoined);
+      client.off('user-left', handleUserLeft);
+      client.unpublish().then(() => client.leave());
+    };
+  }, []);
+
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 200px)',
+        }}
+      >
+        {users.map((user) => (
+          <VideoPlayer key={user.uid} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
