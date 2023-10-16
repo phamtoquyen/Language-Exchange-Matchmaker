c3f5169bc7fe8830046a9374a1808cff3f392e10
bm <bryson.moore@gatech.edu>
2023-10-16 18:28:42 -0400

    Update video chat page styling to match rest of app

--- Frontend/src/Components/Videocall.js
+++ Frontend/src/Components/Videocall.js
@@ -1,21 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
 import './Videocall.css';
 import { VideoRoom } from './VideoRoom';
+import Button from 'react-bootstrap/Button';
+import { createSearchParams, useSearchParams, useNavigate } from "react-router-dom";
 
 function Videocall() {
   const [joined, setJoined] = useState();
+
+  const navigate = useNavigate();
+  const[search] = useSearchParams();
+  const id = search.get("id");
+  const handleBack = async(e) => {
+    navigate({
+        pathname: "/Dashboard",
+        search: createSearchParams({
+            id: id
+        }).toString()
+    });
+  }
+
   return (
-    <div className="App">
-      <h1>Video Call</h1>
-
-      {!joined && (
-        <button onClick={() => setJoined(true)}>
-          Join Room
-        </button>
-      )}
-      {joined && <VideoRoom />}
-
+    <div className="screen-Background">
+      <div className="screen-Container">
+        <div className="screen-Content">
+          <h1>Video Call</h1>
+            {!joined && (
+              <Button className='btn-Screen' onClick={() => setJoined(true)}>
+                Join Room
+              </Button>
+            )}
+            {joined && <VideoRoom />}
+        </div>
+        <Button className="btn-help" onClick={handleBack} >back</Button>
+      </div>
     </div>
   );
 }
